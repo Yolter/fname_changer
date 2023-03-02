95f164579e677ccf9a151dd5af5d3048d350eec0
Ivan <prigulnov88@gmail.com>
2023-03-02 17:18:48 +0500

    преобразвание файла в словарь

---
+++ fname_changer/renamer.py
@@ -0,0 +1,12 @@
+def translit(file):
+    translit_dict = {}
+    flow = open(file).read().splitlines()
+    for line in flow:
+        line = line.split('\t')
+        translit_dict[line[0]] = line[1]
+    return translit_dict
+
+
+if __name__ == '__main__':
+    b = translit('/home/yolter/Dev/fname_changer/transliteration.txt')
+    print(b)
