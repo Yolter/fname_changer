ae3ebfbed7d89c93a161e6c053121de74d3e48c5
Ivan <prigulnov88@gmail.com>
2023-03-06 16:26:15 +0500

    рабочая версия функции renamer, для исключения повторяющегося кода добавлена функция translator_ru_en

--- fname_changer/renamer.py
+++ fname_changer/renamer.py
@@ -2,67 +2,65 @@ from pathlib import Path
 import taglib
 
 
-def translit(file: str) -> dict:
+def table_convert(file: str) -> dict:
     """преобразовывает файл с транслитерацией в словарь"""
     with open(file) as flow:
         # удаляем символ переноса строки '\n'
         flow = flow.read().splitlines()
         # создаем словарь транслитерации в нижнем регистре
-        translit_dict = {
+        translit = {
             ru: en for ru, en in [line.split('\t') for line in flow]
         }
         # создаем словарь транслитерации в верхнем регистре
-        up_translit_dict = {
-            RU.upper(): translit_dict[RU].upper() for RU in translit_dict
+        up_translit = {
+            RU.upper(): translit[RU].upper() for RU in translit
             if RU != 'ь' and RU != 'ъ'
         }
         # объединяем оба словаря
-        translit_dict.update(up_translit_dict)
-        return translit_dict
+        translit.update(up_translit)
+        return translit
+
+
+def translator_ru_en(content: str, table_ru_en: dict) -> str:
+    """кириллические символы заменяет латинскими из таблицы транслитерации,
+     непереводимые символы (ь, ъ) удаляет, символ "_" заменяет на пробел"""
+    content = list(content)
+    for letter in content:
+        if letter in table_ru_en:
+            if letter == 'ь' or letter == 'ъ':
+                content[content.index(letter)] = ''
+            else:
+                content[content.index(letter)] = table_ru_en[letter]
+        elif letter == '_':
+            content[content.index(letter)] = ' '
+    content = ''.join(content)
+    return content
 
 
 def renamer(folder: str, translit_table: dict) -> None:
-    """если имя файла содержит русские буквы, заменяет их транслитерацией"""
+    """если имя файла или теги содержат кириллические символы,
+     заменяет их латинской транслитерацией
+     атрибут folder ожидает абсолютный путь каталога, содержащий файлы .mp3
+     """
     # проверяем существование указанного пути
     if Path(folder).exists():
         # получаем путь каждого файла
         for link in Path(folder).glob('**/*.mp3'):
             # переименовываем русское название файла в транслит
-            file_name = list(Path(link).stem)
-            for letter in file_name:
-                if letter in translit_table:
-                    file_name[file_name.index(letter)] = translit_table[letter]
-                    if letter == 'ь' or letter == 'ъ':
-                        file_name[file_name.index(letter)] = ''
-                elif letter == '_':
-                    file_name[file_name.index(letter)] = ' '
-            file_name = ''.join(file_name)
+            file_name = translator_ru_en(Path(link).stem, translit_table)
             new_link = f'{Path(link).parent}/{file_name}{Path(link).suffix}'
             link.rename(new_link)
             # переименовываем руксские теги файла в транслит
-            song = taglib.File(link)
-            for tag in song.tags.values():
-                tag = list(''.join(tag))
-                for letters in tag:
-                    if letters in translit_table:
-                        tag[tag.index(letters)] = translit_table[letters]
-                        if letters == 'ь' or letters == 'ъ':
-                            tag[tag.index(letters)] = ''
-                    elif letters == '_':
-                        tag[tag.index(letters)] = ' '
-                tag = ''.join(tag)
-
-# вывести кусок кода с переинованием в отдельную функцию и закончить с тегами,
-# остановился на изменении тега внутри цикла после его переименования
-
-
-
-
-
-
+            song = taglib.File(new_link)
+            for tag in song.tags:
+                song.tags[tag] = [translator_ru_en(''.join(song.tags[tag]),
+                                                   translit_table)]
+            song.save()
+            song.close()
 
 
 if __name__ == '__main__':
-    translit_dict = translit('/home/yolter/Dev/fname_changer/transliteration_ru_en.txt')
-    print(translit_dict)
-    renamer('/home/yolter/Downloads/тест', translit_dict)
+    transliter = table_convert(
+        '/home/yolter/Dev/fname_changer/transliteration_ru_en.txt')
+    # print(transliter)
+    renamer('/home/yolter/Downloads/тест', transliter)
